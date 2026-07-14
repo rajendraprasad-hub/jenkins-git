c4e925f135545b78be420b613f5ddd96b3cda5b6
rajendraprasad-hub <rp577514@gmail.com>
2026-07-15 00:12:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Jenkinsrajendra + Node.js!');
+  res.send('Hello Jenkins + Node.js!');
 });
 
 module.exports = app;
